b9ea79974a72cfb8ff72112177c4d714bdd396c4
Sheev <ericmarcypro@gmail.com>
2023-01-30 14:20:52 -0500

    added pseudocode for Page, imported Page to App

---
+++ portfolio/src/components/Page.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+// import other components here
+
+function Page() {
+
+    return(
+        {/* <header className="App-header">
+            <h1> My Name, as title for the portfolio </h1>
+            (header and navbar featured over a professional banner image, using CSS)
+            <nav> 
+                Navbar, including About Me, Portfolio, Resume, and Contact Info sections
+            </nav>
+
+            <img src={portrait} className="profile-picture" alt="portrait" />
+            ^ professional profile photo featuring myself as the portfolio site's subject
+        </header> */}
+    );
+}
+
+
+
